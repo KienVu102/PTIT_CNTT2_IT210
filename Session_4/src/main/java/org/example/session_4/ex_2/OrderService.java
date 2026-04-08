a30ce84ccdfe3e347ddb1327d8b348b1f9453acb
KienVu102 <vudinhkien1002@gmail.com>
2026-04-09 00:56:06 +0700

    Update OrderService.java

--- Session_4/src/main/java/org/example/session_4/ex_2/OrderService.java
+++ Session_4/src/main/java/org/example/session_4/ex_2/OrderService.java
@@ -3,7 +3,6 @@ package org.example.session_4.ex_2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-// Thêm tên Bean "orderServiceEx2" để không đụng hàng với Bài 1
 @Service("orderServiceEx2")
 public class OrderService {
 
