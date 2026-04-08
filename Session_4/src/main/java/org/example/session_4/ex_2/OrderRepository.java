e461d6bc7b78e6fded51e4c227a5d24a3f7119dd
KienVu102 <vudinhkien1002@gmail.com>
2026-04-09 00:55:57 +0700

    Update OrderRepository.java

--- Session_4/src/main/java/org/example/session_4/ex_2/OrderRepository.java
+++ Session_4/src/main/java/org/example/session_4/ex_2/OrderRepository.java
@@ -2,7 +2,6 @@ package org.example.session_4.ex_2;
 
 import org.springframework.stereotype.Repository;
 
-// Thêm tên Bean "orderRepositoryEx2" để không đụng hàng với Bài 1
 @Repository("orderRepositoryEx2")
 public class OrderRepository {
 
